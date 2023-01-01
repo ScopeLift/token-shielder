30c9c781f346856dccb305f2c1821c6c7d42ecc1
alexkeating <keating.dev@protonmail.com>
2023-01-01 14:07:44 -0500

    Main page scaffold

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,39 @@
-import "../styles/globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
+import { ChakraProvider } from "@chakra-ui/provider";
+import { Grid, GridItem } from "@chakra-ui/layout";
+import { extendTheme } from "@chakra-ui/theme-utils";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { RainbowKitProvider, getDefaultWallets } from "@rainbow-me/rainbowkit";
 import { configureChains, createClient, WagmiConfig } from "wagmi";
-import { mainnet, polygon, optimism, arbitrum, goerli } from "wagmi/chains";
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  bsc,
+  goerli,
+} from "wagmi/chains";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
+
+import Header from "../components/Header";
+
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
+
+const theme = extendTheme({ colors });
 
 const { chains, provider, webSocketProvider } = configureChains(
   [
     mainnet,
+    bsc,
     polygon,
-    optimism,
     arbitrum,
     ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" ? [goerli] : []),
   ],
@@ -24,7 +46,7 @@ const { chains, provider, webSocketProvider } = configureChains(
 );
 
 const { connectors } = getDefaultWallets({
-  appName: "RainbowKit App",
+  appName: "Railgun Shielder App",
   chains,
 });
 
@@ -37,11 +59,37 @@ const wagmiClient = createClient({
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains}>
-        <Component {...pageProps} />
-      </RainbowKitProvider>
-    </WagmiConfig>
+    <>
+      <Head>
+        <title>Railgun Shielder App</title>
+        <meta
+          name="description"
+          content="An app to shield funds into railgun"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains}>
+          <ChakraProvider theme={theme}>
+            <Grid
+              templateAreas={`". header ."
+                  ". body ."`}
+              gridTemplateRows={"4.8rem 100vh"}
+              gridTemplateColumns={"1fr minmax(auto, 150rem) 1fr"}
+              gap="1"
+              marginX="1rem"
+            >
+              <GridItem area={"header"}>
+                <Header />
+              </GridItem>
+              <GridItem area={"body"}>
+                <Component {...pageProps} />
+              </GridItem>
+            </Grid>
+          </ChakraProvider>
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </>
   );
 }
 
