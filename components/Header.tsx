2c1c5f1ca2ea0d76f358ede0bfaa59dfa1c5b122
Alexander Keating <keating.dev@protonmail.com>
2023-01-03 21:55:17 -0500

    Main page scaffold (#5)
    * Main page scaffold
    
    * Make changes based on feedback
    
    * Replace logo with protect icon

---
+++ components/Header.tsx
@@ -0,0 +1,19 @@
+import { Image } from "@chakra-ui/image";
+import { Flex } from "@chakra-ui/layout";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+// TODO: Logo is a placeholder until we have web assets
+const Header = () => {
+  return (
+    <Flex align="center" justify="space-between" h="100%">
+      <Image
+        boxSize="2.5rem"
+        src="protect-icon.svg"
+        alt="Railgun shield logo"
+      />
+      <ConnectButton />
+    </Flex>
+  );
+};
+
+export default Header;
