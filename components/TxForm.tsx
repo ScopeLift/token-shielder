105a7f09cc06aac0f608ce77d9b969c6df0177d2
Ed Mazurek <Edward.R.Mazurek@gmail.com>
2023-01-16 15:01:33 -0500

    Initial railgun quickstart sdk integration (#34)
    * install sdk
    
    * wip: paste docs example
    
    * wip: working gas estimate
    
    * do tx
    
    * txform refactor
    
    * useShieldPrivateKey
    
    * ts-ignore
    
    * do erc20 approval every time

--- components/TxForm.tsx
+++ components/TxForm.tsx
@@ -1,15 +1,140 @@
 import { useToken } from "@/contexts/TokenContext";
 import useNotifications from "@/hooks/useNotifications";
+import useShieldPrivateKey from "@/hooks/useShieldPrivateKey";
 import { Button } from "@chakra-ui/button";
 import { FormControl, FormLabel } from "@chakra-ui/form-control";
 import { Input, InputGroup, InputRightElement } from "@chakra-ui/input";
 import { Box, Flex } from "@chakra-ui/layout";
 import { Select } from "@chakra-ui/select";
+import {
+  gasEstimateForShield,
+  getRailgunSmartWalletContractForNetwork,
+  populateShield,
+} from "@railgun-community/quickstart";
+import {
+  RailgunERC20AmountRecipient,
+  NetworkName,
+  TransactionGasDetailsSerialized,
+  EVMGasType,
+  NETWORK_CONFIG,
+  deserializeTransaction,
+} from "@railgun-community/shared-models";
+import { erc20ABI } from "@wagmi/core";
+import { ethers, constants } from "ethers";
+import { useState } from "react";
+import {
+  useAccount,
+  useSigner,
+  usePrepareContractWrite,
+  useContractWrite,
+  useProvider,
+} from "wagmi";
 
 export const TxForm = () => {
   // TODO: Placeholder notification for shielding
   const { tokenList } = useToken();
-  const { notifyUser } = useNotifications();
+  const network = NetworkName.EthereumGoerli;
+  const { txNotify } = useNotifications();
+  let abi = [
+    "function transfer(address,uint256) returns (bool)",
+    "function approve(address,uint256) returns (bool)",
+  ];
+  console.log(
+    getRailgunSmartWalletContractForNetwork(NetworkName.EthereumGoerli).address
+  );
+  const { isConnected, address } = useAccount();
+  const { data: signer } = useSigner();
+  const provider = useProvider();
+  const { getShieldPrivateKey } = useShieldPrivateKey();
+  const { config } = usePrepareContractWrite({
+    address: "0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6",
+    abi: erc20ABI,
+    functionName: "approve",
+    args: [
+      getRailgunSmartWalletContractForNetwork(network).address as `0x{string}`,
+      constants.MaxUint256,
+    ],
+  });
+  const { writeAsync: doErc20Approval } = useContractWrite(config);
+  const [recipient, setRecipient] = useState<string>(
+    "0zk1qyn0qa5rgk7z2l8wyncpynmydgj7ucrrcczhl8k27q2rw5ldvv2qrrv7j6fe3z53ll5j4fjs9j5cmq7mxsaulah7ykk6jwqna3nwvxudp5w6fwyg8cgwkwwv3g4"
+  );
+  const [tokenAddress, setTokenAddress] = useState<string>();
+  const [tokenAmount, setTokenAmount] = useState<string>("");
+  const [tokenDecimals, setTokenDecimals] = useState<number>();
+
+  const doSubmit: React.FormEventHandler = async (e) => {
+    // TODO: Form validation
+    // Formatted token amounts and recipients.
+    const erc20AmountRecipients: RailgunERC20AmountRecipient[] = [
+      {
+        tokenAddress: tokenAddress!,
+        amountString: ethers.utils
+          .parseUnits(tokenAmount!, tokenDecimals)
+          .toHexString(), // must be hex
+        recipientAddress: recipient!, // RAILGUN address
+      },
+    ];
+
+    // The shieldPrivateKey enables the sender to decrypt
+    // the receiver's address in the future.
+    const shieldPrivateKey = await getShieldPrivateKey();
+
+    if (!doErc20Approval) throw "not prepared";
+    await doErc20Approval();
+
+    // Public wallet to shield from.
+    const fromWalletAddress = address as `0x{string}`;
+
+    const { gasEstimateString, error: err } = await gasEstimateForShield(
+      network,
+      shieldPrivateKey,
+      erc20AmountRecipients,
+      [], // nftAmountRecipients
+      fromWalletAddress
+    );
+
+    if (err) {
+      throw err;
+    }
+
+    const { maxFeePerGas, maxPriorityFeePerGas } = await provider.getFeeData();
+    const gasDetailsSerialized: TransactionGasDetailsSerialized = {
+      evmGasType: EVMGasType.Type2, // Depends on the chain (BNB uses type 0)
+      gasEstimateString: gasEstimateString!, // Output from gasEstimateForShield
+      maxFeePerGasString: maxFeePerGas!.toHexString(), // Current gas Max Fee
+      maxPriorityFeePerGasString: maxPriorityFeePerGas!.toHexString(), // Current gas Max Priority Fee
+    };
+
+    const { serializedTransaction, error } = await populateShield(
+      network,
+      shieldPrivateKey,
+      erc20AmountRecipients,
+      [], // nftAmountRecipients
+      gasDetailsSerialized
+    );
+    if (error) {
+      throw error;
+    }
+
+    const { chain } = NETWORK_CONFIG[network];
+
+    const transactionRequest: ethers.providers.TransactionRequest =
+      deserializeTransaction(
+        serializedTransaction as string,
+        undefined, // nonce (optional)
+        chain.id
+      );
+
+    // Public wallet to shield from.
+    transactionRequest.from = address;
+    // TODO: handle transaction status and notifications in a feature
+    const tx = await signer?.sendTransaction(transactionRequest);
+    await tx?.wait().then(() => {
+      txNotify(tx.hash);
+    });
+  };
+
   return (
     <Box width="24rem">
       <FormControl>
@@ -18,18 +143,29 @@ export const TxForm = () => {
           type="string"
           variant="outline"
           size="lg"
-          placeholder="vitalik.eth"
           pr="4.5rem"
           height="4rem"
           mb=".75rem"
+          value={recipient}
+          onChange={(e) => setRecipient(e.target.value)}
         />
       </FormControl>
       <FormControl>
         <FormLabel>Token</FormLabel>
-        <Select size="lg" height="4rem" mb=".75rem">
-          {tokenList.map((item) => {
+        <Select
+          size="lg"
+          height="4rem"
+          mb=".75rem"
+          onChange={(e) => {
+            const { address, decimals } = tokenList[+e.target.value];
+            setTokenAddress(address);
+            setTokenDecimals(decimals);
+          }}
+        >
+          <option></option>
+          {tokenList.map((item, i) => {
             return (
-              <option key={item.name} value={item.name}>
+              <option key={item.name} value={i}>
                 {item.name}
               </option>
             );
@@ -43,9 +179,10 @@ export const TxForm = () => {
             type="number"
             variant="outline"
             size="lg"
-            placeholder="0"
             pr="4.5rem"
             height="100%"
+            value={tokenAmount}
+            onChange={(e) => setTokenAmount(e.target.value)}
           />
           <InputRightElement width="4.5rem" height="100%">
             <Button size="sm">Max</Button>
@@ -53,17 +190,7 @@ export const TxForm = () => {
         </InputGroup>
         <Flex justify="flex-end"></Flex>
       </FormControl>
-      <Button
-        size="lg"
-        mt="1rem"
-        width="100%"
-        onClick={() => {
-          notifyUser({
-            alertType: "success",
-            message: "Token was shielded successfully",
-          });
-        }}
-      >
+      <Button size="lg" mt="1rem" width="100%" onClick={doSubmit}>
         Shield
       </Button>
     </Box>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,26 @@
 import HowTo from "@/components/HowTo";
 import { TxForm } from "@/components/TxForm";
+import { loadProviders } from "@/utils/railgun";
 import { Flex, Heading } from "@chakra-ui/layout";
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useEffect, useState } from "react";
 
 const Home: NextPage = () => {
+  const [isProviderLoaded, setProviderLoaded] = useState<Boolean>(false);
+  useEffect(() => {
+    const fn = async () => {
+      await loadProviders();
+      setProviderLoaded(true);
+    };
+    fn();
+  }, []);
   return (
     <Flex direction="column" align="center" justify="center" pt="2rem">
       <Heading as="h1" size="4xl" mb="4rem">
         Shield
       </Heading>
-      <TxForm />
+      {isProviderLoaded && <TxForm />}
       <HowTo />
     </Flex>
   );
