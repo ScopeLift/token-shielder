6c0be104757d49e0d719cbf93df12526795127ba
keating <keating.dev@protonmail.com>
2023-01-24 14:05:03 -0500

    Only fetch token list once

--- hooks/useTokenList.tsx
+++ hooks/useTokenList.tsx
@@ -1,6 +1,6 @@
 import { ethAddress } from "@/utils/constants";
 import { networks } from "@/utils/networks";
-import useSWR from "swr";
+import useSWRImmutable from "swr/immutable";
 import { useNetwork } from "wagmi";
 
 export interface TokenListItem {
@@ -16,28 +16,34 @@ interface TokenListJson {
   tokens: TokenListItem[];
 }
 
+const useTokenGet = () => {
+  const { isLoading, error, data } = useSWRImmutable(
+    `userTokenList`,
+    async () => {
+      const resp = await fetch("tokenlist.json");
+      const json = (await resp.json()) as TokenListJson;
+      return json;
+    }
+  );
+  return { isLoading, error, data };
+};
+
 export const useTokenList = () => {
   const { chain } = useNetwork();
   const chainId = chain?.id || 1; // default to mainnet if no chain id
   const network = networks[chainId];
-  const { isLoading, error, data } = useSWR(
-    `userTokenList-${chainId}`,
-    async () => {
-      const resp = await fetch("tokenlist.json");
-      const json = (await resp.json()) as TokenListJson;
-      const tokenList = json.tokens.filter(
-        (token) => token.chainId === chainId
-      );
-      const baseToken: TokenListItem = {
-        chainId,
-        symbol: network.baseToken.symbol,
-        address: ethAddress,
-        decimals: 18,
-        name: network.baseToken.name,
-        logoURI: network.baseToken.logoURI,
-      };
-      return { tokenList: [baseToken, ...tokenList] };
-    }
-  );
-  return { isLoading, error, tokenList: data?.tokenList };
+  const { data, isLoading, error } = useTokenGet();
+  if (!data) {
+    return { isLoading, error, tokenList: [] };
+  }
+  const tokenList = data.tokens.filter((token) => token.chainId === chainId);
+  const baseToken: TokenListItem = {
+    chainId,
+    symbol: network.baseToken.symbol,
+    address: ethAddress,
+    decimals: 18,
+    name: network.baseToken.name,
+    logoURI: network.baseToken.logoURI,
+  };
+  return { isLoading, error, tokenList: [baseToken, ...tokenList] };
 };
