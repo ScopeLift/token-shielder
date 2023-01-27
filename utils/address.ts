8e312046b87f4d4ad5ee5370b0bbe7ba95a30ae7
keating <keating.dev@protonmail.com>
2023-01-27 17:06:15 -0500

    Remove unused import

--- utils/address.ts
+++ utils/address.ts
@@ -1,5 +1,3 @@
-import { assertValidRailgunAddress } from '@railgun-community/quickstart';
-
 export const shortenAddress = (address: string) => {
   return `${address.slice(0, 7)}...${address.slice(-4)}`;
 };
