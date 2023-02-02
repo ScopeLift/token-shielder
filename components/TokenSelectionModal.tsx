710b691261f962c916d3084d69aa58476c86367b
keating <keating.dev@protonmail.com>
2023-02-02 15:45:14 -0500

    Make changes based on comments

--- components/TokenSelectionModal.tsx
+++ components/TokenSelectionModal.tsx
@@ -23,8 +23,7 @@ import WarningModal from '@/components/WarningModal';
 import { useToken } from '@/contexts/TokenContext';
 import { TokenListContextItem } from '@/contexts/TokenContext';
 import useNotifications from '@/hooks/useNotifications';
-import blacklistJson from '@/public/blacklist.json';
-import { ipfsDomain } from '@/utils/constants';
+import { ipfsDomain, rebaseTokens } from '@/utils/constants';
 import { parseIPFSUri } from '@/utils/ipfs';
 import { networks } from '@/utils/networks';
 
@@ -137,7 +136,7 @@ const CustomTokenSelectionItem = ({
     token: tokenAddress,
     chainId: chain?.id,
   });
-  const isBlacklisted = blacklistJson.find(
+  const isBlacklisted = rebaseTokens.find(
     (address) => tokenAddress.toLowerCase() === address.toLowerCase()
   );
   const openModal = isBlacklisted ? onBlacklistOpen : onCustomOpen;
@@ -213,7 +212,7 @@ const CustomTokenSelectionItem = ({
             onBlacklistClose();
           }}
           address={tokenAddress}
-          description={'This token is blacklisted and cannot be shielded!'}
+          description={'This token cannot be shielded because it is a rebase token'}
         />
       </>
     );
