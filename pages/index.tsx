a5219dd19190135d76469e749286fb767ba9d4aa
Keating <keating.dev@protonmail.com>
2023-03-16 21:42:44 -0400

    Add dwarf image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,21 @@
+import { Image } from '@chakra-ui/image';
 import { Flex, Heading } from '@chakra-ui/layout';
 import { TxForm } from '@/components/TxForm';
 
 const Home = ({ recipientAddress }: { recipientAddress?: string }) => {
   return (
-    <Flex direction="column" align="center" justify="center" pt="2rem">
+    <Flex direction="column" align="center" justify="center">
       <Heading as="h1" size="2xl" mb="1rem">
         TokenShielder
       </Heading>
-      <Heading as="h4" size="xs" mb="3rem">
+      <Image
+        boxSize="10rem"
+        src="dwarf.jpg"
+        alt="dwarf with shield"
+        borderRadius="8rem"
+        mb="1.5rem"
+      />
+      <Heading as="h4" size="xs" mb="2rem">
         Use your crypto wallet of choice to transfer tokens directly into a RAILGUN Private 0zk
         balance.
       </Heading>
