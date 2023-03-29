a16f6e8178f6a3a90c9d6ae87192ef6362fdc96b
wildmolasses <changes@gmail.com>
2023-03-28 19:38:29 -0700

    open how it works modal if first session

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,27 @@
+import { useEffect } from 'react';
 import { ExternalLinkIcon } from '@chakra-ui/icons';
 import { Flex, Link } from '@chakra-ui/layout';
 import { useDisclosure } from '@chakra-ui/react';
+import localforage from 'localforage';
 import { HowItWorksModal } from '@/components/HowItWorks';
 
 const Footer = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  useEffect(() => {
+    const openModalIfFirstSession = async () => {
+      return localforage.getItem('seen').then((isNotFirstSession) => {
+        const isFirstSession = !isNotFirstSession;
+        if (isFirstSession) {
+          onOpen();
+          localforage.setItem('seen', true);
+        }
+      });
+    };
+
+    openModalIfFirstSession();
+  }, [onOpen]);
+
   return (
     <Flex h="100%" gap="1rem" align="center">
       <Link size="sm" cursor="pointer" onClick={onOpen}>
