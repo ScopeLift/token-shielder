6ecfb6d476f34b735ca056c3a809f4325222f094
Ed Mazurek <Edward.R.Mazurek@gmail.com>
2023-03-31 09:23:54 -0700

    Fixed wallet connect to top (#134)

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,12 +2,15 @@ import { Image } from '@chakra-ui/image';
 import { Flex } from '@chakra-ui/layout';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 
-// TODO: Logo is a placeholder until we have web assets
 const Header = () => {
   return (
-    <Flex align="center" justify="space-between" h="100%">
-      <Image boxSize="2.5rem" src="protect-icon.svg" alt="Shield logo" />
-      <ConnectButton />
+    <Flex as="header" mt="4">
+      <Flex align="center" justify="space-between" h="100%">
+        <Image boxSize="2.5rem" src="protect-icon.svg" alt="Shield logo" />
+      </Flex>
+      <Flex position="fixed" w="98%" justify="flex-end">
+        <ConnectButton />{' '}
+      </Flex>
     </Flex>
   );
 };
