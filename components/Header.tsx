82c74d762bd0ba263e8d56bc5835d8ac5966d1fc
bbist <pro.bbist@gmail.com>
2023-05-22 11:52:24 +0545

    railgun wallet selection menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,19 @@
 import { Image } from '@chakra-ui/image';
-import { Flex } from '@chakra-ui/layout';
+import { Flex, VStack } from '@chakra-ui/layout';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { SelectRailgunWallet } from './RailgunWallet';
 
 const Header = () => {
   return (
-    <Flex as="header" mt="4">
-      <Flex align="center" justify="space-between" h="100%">
+    <Flex as="header" mt="4" justify="space-between">
+      <Flex>
         <Image boxSize="2.5rem" src="protect-icon.svg" alt="Shield logo" />
       </Flex>
-      <Flex position="fixed" w="98%" justify="flex-end">
-        <ConnectButton />{' '}
+      <Flex>
+        <VStack align="end">
+          <ConnectButton />
+          <SelectRailgunWallet />
+        </VStack>
       </Flex>
     </Flex>
   );
