dd7fc72c6d10944cc82432b8a61d131f90069551
bbist <pro.bbist@gmail.com>
2023-05-23 13:25:17 +0545

    add integrations menu in header and add aave integrations page

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,79 @@
+import { useState } from 'react';
+import Link from 'next/link';
+import { HamburgerIcon, LockIcon } from '@chakra-ui/icons';
 import { Image } from '@chakra-ui/image';
 import { Flex, VStack } from '@chakra-ui/layout';
+import {
+  Button,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItemOption,
+  MenuList,
+  MenuOptionGroup,
+  Text,
+  Tooltip,
+} from '@chakra-ui/react';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { SelectRailgunWallet } from './RailgunWallet';
 
+
+const INTEGRATIONS = [
+  {
+    id: 'aave-v3',
+    name: 'Aave V3',
+    description: 'Deposit, withdraw, borrow and repay using AAVE money market protocol',
+    href: '/integrations/aave-v3',
+  },
+];
+
+
 const Header = () => {
+  const [integration, setIntegration] = useState(INTEGRATIONS[0]);
+
   return (
     <Flex as="header" mt="4" justify="space-between">
       <Flex>
-        <Image boxSize="2.5rem" src="protect-icon.svg" alt="Shield logo" />
+        <Menu matchWidth closeOnSelect={false}>
+          <MenuButton variant="outline" as={IconButton} icon={<HamburgerIcon />} />
+          <MenuList>
+            <MenuItem as={Link} value="wallet" href="/wallet">
+              <Tooltip
+                hasArrow
+                fontSize="sm"
+                placement="auto-start"
+                label={
+                  'Shield, Unshield and Transfer tokens between your public address and RAILGUN Private 0zk address'
+                }
+              >
+                <Button variant="outline" w="100%" aria-label="Railgun Wallet" leftIcon={<LockIcon />}>
+                  Wallet
+                </Button>
+              </Tooltip>
+            </MenuItem>
+            <MenuOptionGroup title="Integrations" value={integration.href}>
+              {INTEGRATIONS.map(({ id, name, description, href }, i) => {
+                return (
+                  <MenuItemOption as={Link} key={i} value={id} href={href}>
+                    <Tooltip hasArrow fontSize="sm" label={description} placement="auto-start">
+                      <Text>{name}</Text>
+                    </Tooltip>
+                  </MenuItemOption>
+                );
+              })}
+            </MenuOptionGroup>
+          </MenuList>
+        </Menu>
+      </Flex>
+      <Flex>
+        <Image
+          position="absolute"
+          left="48%"
+          boxSize="2.5rem"
+          src="/protect-icon.svg"
+          alt="Shield logo"
+        />
       </Flex>
       <Flex>
         <VStack align="end">
